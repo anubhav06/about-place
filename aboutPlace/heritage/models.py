5c379f624cdd6bb314432d5249e6108a5dcde452
Anubhav Gupta <mail.anubhav06@gmail.com>
2021-10-23 01:49:10 +0530

    Minor words renamed

--- aboutPlace/heritage/models.py
+++ aboutPlace/heritage/models.py
@@ -3,8 +3,6 @@ from django.db import models
 
 class User(AbstractUser):
     
-    #followers = models.ManyToManyField('User', blank=True)
     def __str__(self):
         return f"{self.username}"
-
 
