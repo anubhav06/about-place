aecd13fb81e83d03a5ef86baae2c94329090065d
Anubhav Gupta <mail.anubhav06@gmail.com>
2021-10-23 23:30:42 +0530

    Changed API route

--- aboutPlace/heritage/static/heritage/script.js
+++ aboutPlace/heritage/static/heritage/script.js
@@ -3,10 +3,12 @@ document.addEventListener('DOMContentLoaded', function(){
     console.log('Loaded!')
 
     var countries = document.querySelectorAll('.countryName');
-    for(var i=0 ; i<countries.length; i++){
+    if( countries.length > 1 ){   
+        for(var i=0 ; i<countries.length; i++){
 
-        var name = countries[i].innerHTML
-        callFetch(name,countries,i)
+            var name = countries[i].innerHTML
+            callFetch(name,countries,i)
+        }
     }
 
 })
@@ -14,10 +16,10 @@ document.addEventListener('DOMContentLoaded', function(){
 
 function callFetch(name, countries, i){
     
-    fetch('https://www.googleapis.com/customsearch/v1?key=AIzaSyA-iusVy-dJ3y56kSEA3GNE-uZRL0lmbyA&cx=9a0610b867724db27&q=' + name)
+    fetch('https://restcountries.com/v3.1/name/' + name)
     .then(response => response.json())
     .then(response => {
-        var url = response.items[0].pagemap.cse_image[0].src
+        var url = response[0].flags.png
         countries[i].innerHTML = "<img src='" + url + "' alt='" + name + "'s flag' height='250' width='auto'>"
     })
 }
