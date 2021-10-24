c3a8b80796a90e0b900cce05350292541ecc7ee9
0rGaan1c <nathan4drake1999@gmail.com>
2021-10-24 11:40:36 +0530

    flag design

--- aboutPlace/heritage/static/heritage/script.js
+++ aboutPlace/heritage/static/heritage/script.js
@@ -1,45 +1,43 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Loaded!");
 
-    console.log('Loaded!')
+  //var countries = document.querySelectorAll('.countryName');
+  //if( countries.length > 1 ){
+  //    for(var i=0 ; i<countries.length; i++){
 
-    
+  //        var name = countries[i].innerHTML
+  //        callFetch(name,countries,i)
+  //    }
+  //}
+  //else{
+  //    console.log('No countries! ')
+  //}
+});
 
-    //var countries = document.querySelectorAll('.countryName');
-    //if( countries.length > 1 ){   
-    //    for(var i=0 ; i<countries.length; i++){
-
-    //        var name = countries[i].innerHTML
-    //        callFetch(name,countries,i)
-    //    }
-    //}
-    //else{
-    //    console.log('No countries! ')
-    //}
-
-})
-
-
-function callFetch(name, countries, i){
-    console.log('Fetch ran')
-    fetch('https://restcountries.com/v3.1/name/' + name)
-    .then(response => response.json())
-    .then(response => {
-        var url = response[0].flags.png
-        countries[i].innerHTML = "<img src='" + url + "' alt='" + name + "'s flag' height='250' width='auto'>"
-    })
+function callFetch(name, countries, i) {
+  console.log("Fetch ran");
+  fetch("https://restcountries.com/v3.1/name/" + name)
+    .then((response) => response.json())
+    .then((response) => {
+      var url = response[0].flags.png;
+      countries[i].innerHTML =
+        "<img src='" +
+        url +
+        "' alt='" +
+        name +
+        "'s flag width=320 height=213'>";
+    });
 }
 
-function loaded(){
-    console.log('Template Loaded')
-    var countries = document.querySelectorAll('.countryName');
-    if( countries.length > 0 ){   
-        for(var i=0 ; i<countries.length; i++){
-
-            var name = countries[i].innerHTML
-            callFetch(name,countries,i)
-        }
+function loaded() {
+  console.log("Template Loaded");
+  var countries = document.querySelectorAll(".countryName");
+  if (countries.length > 0) {
+    for (var i = 0; i < countries.length; i++) {
+      var name = countries[i].innerHTML;
+      callFetch(name, countries, i);
     }
-    else{
-        console.log('No countries! ')
-    }
+  } else {
+    console.log("No countries! ");
+  }
 }
